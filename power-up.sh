acd5fb755e751c3ce24b0817601ec5c1e744be63
Amr Monier <43762965+AmrMonier@users.noreply.github.com>
2023-01-28 20:19:59 +0200

    uncomment scripts

--- power-up.sh
+++ power-up.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# sudo ./scripts/essential-packages.sh
+ sudo ./scripts/essential-packages.sh
 
-# sudo ./scripts/snap.sh
+ sudo ./scripts/snap.sh
 
-# sudo ./scripts/web-browsers.sh
+ sudo ./scripts/web-browsers.sh
 
-# sudo ./scripts/beekeeper.sh
+ sudo ./scripts/beekeeper.sh
 
-# sudo ./scripts/install-nodejs.sh
+ sudo ./scripts/install-nodejs.sh
 
-# sudo ./scripts/zsh.sh
+ sudo ./scripts/zsh.sh
 
-# sudo ./scripts/docker.sh
+ sudo ./scripts/docker.sh
 
 # # this should be the last step
 sudo ./scripts/aliases.sh
